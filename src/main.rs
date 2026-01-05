2bfee08d0be93d1320abf2dd3006ab6ab4d98fe2
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2026-01-05 13:30:49 -0500

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,20 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    let mut x = true;
-    while x{
+    loop     
+    {
     let mut command = String::new();
     print!("$ ");
     io::stdout().flush().unwrap();
 
     io::stdin().read_line(&mut command).unwrap();
-    if command == ""{
-        x = false
+    if command.is_empty(){
+        continue;
+    } 
+    if command == "exit"{
+        break;
     }
-    else{
     println!("{}: command not found", command.trim());
-    }
 
     }
 }
