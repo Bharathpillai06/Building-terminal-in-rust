ce74f0d2e6f3607f639154fedebbd6ad56e8f37f
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2026-01-05 13:50:05 -0500

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         break;
     }
     if &command.trim_left()[0..4] == "echo"{
-        println!("{}",&command[4..].trim_left());
+        print!("{}",&command[4..].trim_left());
         continue;
         
     }
